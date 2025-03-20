0338be5eac4a09277085a3e0472897365023535a
hossain-murad <dev.hossainmurad@gmail.com>
2025-03-21 02:34:59 +0900

    added ohmyposh

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -18,6 +18,8 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "lazyvim.plugins.extras.lang.java" },
+    { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
     -- Override the tokyonight.nvim plugin configuration
